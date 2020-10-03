e94fd33e3a9ff0f7edb1fcd716fcddef09f7e929
Filipe Fonseca <62034518+Filipe-create@users.noreply.github.com>
2020-10-03 11:22:32 -0300

    commit p1 de poo fatec

---
+++ pacote-download/filipe_p1_2020_09_28/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace filipe_p1_2020_09_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            int opcao;
+            do
+            {
+                Console.WriteLine("Proporção padrão para piscinas (4 gramas de cloro para cada 1000 litros de água).");
+
+                Console.Write("O que deseja calcular, caixa d'água ou piscina? ");
+                string compartimento = Console.ReadLine();
+
+                Console.Write("Informe a quantidade de água em litros...: ");
+                double quantidadeAguaLitros = double.Parse(Console.ReadLine());
+
+                if (quantidadeAguaLitros < 10)
+                    Console.WriteLine("Valor muito baixo para ser calculado.");
+                else {
+                    double quantidadeCloroSerUsado = quantidadeAguaLitros * 4 / 1000;
+
+                    Console.WriteLine("A quantidade de cloro a ser usada é...: {0:n2} gramas de cloro.", quantidadeCloroSerUsado);
+
+                    Console.WriteLine("para sua " + compartimento);
+                }
+                Console.Write("1 - novo cálculo\nPressione qualquer tecla para sair: ");
+                    opcao = int.Parse(Console.ReadLine());      
+            }
+            while (opcao == 1);            
+
+            Console.ReadKey();            
+        }
+    }
+}
